5dd17ea086d97f4ed399e3f3d214dcb727990fb7
VV00nder <gio.baranikovi@gmail.com>
2025-12-16 21:30:40 +0300

    fixed

--- joke-server/main.go
+++ joke-server/main.go
@@ -7,18 +7,18 @@ import (
 	"time"
 )
 
-var jokes = []string{
-	"Почему программисты путают Хэллоуин и Рождество? Потому что Oct 31 == Dec 25.",
-	"Алгоритм шуток: сначала сделай что-то смешное, потом оптимизируй.",
-	"Программист не боится темноты — он боится пустого Stack Overflow.",
-	"— Ты где? — Я в Go. — В каком? — В конкурентном.",
-}
-
 func jokeHandler(w http.ResponseWriter, r *http.Request) {
-	rand.Seed(time.Now().UnixNano())
+	var jokes = []string{
+		"Почему программисты путают Хэллоуин и Рождество? Потому что Oct 31 == Dec 25.",
+		"Алгоритм шуток: сначала сделай что-то смешное, потом оптимизируй.",
+		"Программист не боится темноты — он боится пустого Stack Overflow.",
+		"— Ты где? — Я в Go. — В каком? — В конкурентном.",
+	}
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	joke := jokes[rand.Intn(len(jokes))]
 
-	tmpl, err := template.ParseFiles("templates/joke.html")
+	tmpl, err := template.ParseFiles("joke-server/templates/joke.html")
 	if err != nil {
 		http.Error(w, "Ошибка шаблона", http.StatusInternalServerError)
 		return
